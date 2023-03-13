5175388a7706013368881a8a87caf00c65321708
Georgiy_Domanin <gdomanin@gmail.com>
2023-03-13 20:17:57 +0300

    countSort added

--- sorts.cpp
+++ sorts.cpp
@@ -107,9 +107,52 @@ void test_inputSort(){
         std::cout << mass[i] << " ";
 }
 
+int get_max(int *mass, int size){
+    int diapason = 0;
+    for (int i = 0; i < size; i++){
+        if (diapason < mass[i])
+            diapason = mass[i];
+    }
+    return diapason;
+}
+
+
+void countSort(int *mass, int size) {
+    int diapason = get_max(mass, size);
+    int *boofmass = new int[diapason];
+    for (int i = 0; i < diapason; i++)
+        boofmass[i] = 0;
+    for (int i = 0; i < size; i++) {
+        boofmass[mass[i]]++;
+    }
+    int numb = 0;
+    for (int i = 0; i <= diapason; i++) {
+        if (boofmass[i] != 0) {
+            for (int j = 0; j < boofmass[i]; j++) {
+                mass[numb] = i;
+                numb++;
+            }
+        }
+    }
+        delete[] boofmass;
+}
+
+void test_countSort(){
+    const int size = 10;
+    int mass[size] = {10, 20, 43, 68, 2, 7, 0, 23, 43, 65};
+    int etalonmass[size] = {0,2 ,7, 10, 20, 23, 43, 43, 65, 68};
+    countSort(mass, size);
+    for (int i = 0; i < size; i++) {
+        if (mass[i] != etalonmass[i]) {
+            std::cout << "bad countSort";
+            break;
+        }
+    }
+    for (int i = 0; i < size; i++)
+        std::cout << mass[i] << " ";
+}
+
 int main() {
-    test_bubbleSort();
-    test_get_max_ind();
-    test_inputSort();
+    test_countSort();
     return 0;
 }
