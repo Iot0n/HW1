45a587556b690250c34ef3d18458d0ba95ba42a6
Georgiy_Domanin <gdomanin@gmail.com>
2023-03-13 17:00:17 +0300

    selectionSort added and bubS. fix

--- sorts.cpp
+++ sorts.cpp
@@ -7,16 +7,17 @@ void swap(int& a, int& b) {
 }
 
 void bubbleSort(int *mass, int size) {
-    bool sorted = false;
-    while (!sorted) {
-        for (int i = 1; i < size; i++) {
-            if (mass[i] < mass[i - 1])
-                swap(mass[i], mass[i - 1]);
-            else {
-                sorted = true;
-                break;
+    bool swapped = false;
+    while (1) {
+        swapped = false;
+        for (int i = 0; i < (size-1) ; i++) {
+            if (mass[i] > mass[i + 1]) {
+                swap(mass[i], mass[i + 1]);
+                swapped = true;
             }
         }
+        if (!swapped)
+            break;
     }
 }
 
@@ -25,10 +26,14 @@ void test_bubbleSort (){
     int mass[size] = {10, 20, 43, 68, 2, 0, 1, 23, 43, 65};
     int etalonmass[size] = {0,1 ,2, 10, 20, 23, 43, 43, 65, 68};
     bubbleSort(mass, size);
-    for (int i = 0; i < size; i++) {
-        if (mass[i] != etalonmass[i])
-            std::cout << "bad sort";
+    for (int i = 0; i < size-1; i++) {
+        if (mass[i] != etalonmass[i]) {
+            std::cout << "bad bubbleSort" << std::endl;
+            break;
+        }
     }
+    for (int i = 0; i < size; i++)
+        std::cout << mass[i] << " ";
 }
 
 int get_max_ind(int *mass, int size){
@@ -45,6 +50,28 @@ void test_get_max_ind(){
     int maxind = get_max_ind(mass, 10);
     if (maxind != 3){
         std::cout << "bad test_get_max";
+    }
+}
+
+void selectionSort(int *mass, int size){
+    int maxind;
+    while (size){
+        maxind = get_max_ind(mass, size);
+        swap(mass[maxind], mass[size]);
+        size--;
+    }
+}
+
+void test_selectionSort (){
+    const int size = 10;
+    int mass[size] = {10, 20, 43, 68, 2, 0, 1, 23, 43, 65};
+    int etalonmass[size] = {0,1 ,2, 10, 20, 23, 43, 43, 65, 68};
+    selectionSort(mass, size);
+    for (int i = 0; i < size; i++) {
+        if (mass[i] != etalonmass[i]) {
+            std::cout << "bad selectionSort";
+            break;
+        }
     }
 }
 
